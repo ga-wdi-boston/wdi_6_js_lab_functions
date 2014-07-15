dae40aab257b61a5caccef151edf0835744663d6
Devin Dreszer <devindreszer@gmail.com>
2014-07-15 18:11:06 -0400

    Project as function to censor bad words

--- comments.js
+++ comments.js
@@ -1,7 +1,12 @@
+content = "what the hell is going on. stupid javascript!";
+badWords = ["hell", "stupid"];
+
 // regularPrint(content) – accepts a string content and prints it to the console
 function regularPrint(content) {
   console.log(content);
 }
+
+regularPrint(content);
 
 // loudPrint(content) – accepts a string content and prints an upcased version
 // of it surrounded by stars (e.g. ***THIS VIDEO SUCKS***)
@@ -9,3 +14,17 @@ function loudPrint(content) {
   console.log("***" + content.toUpperCase() + "***");
 }
 
+loudPrint(content);
+
+// omissionCensor(content, badWords) – accepts a string content and an array of
+// strings badWords, and returns a version of content with all instances of the
+// strings in badWords removed
+function omissionCensor(content, badWords) {
+  var words = content.split(" ");
+  var censored = words.filter(function(word){
+    return badWords.indexOf(word) === -1;
+  });
+  return censored.join(" ");
+}
+
+console.log(omissionCensor(content, badWords));
