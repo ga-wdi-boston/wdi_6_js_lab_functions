4579a42ed276efc7787a37f6386c2f69a9782983
csb324 <csb324@gmail.com>
2014-07-15 17:23:37 -0400

    regularPrint

--- comments.js
+++ comments.js
@@ -1,1 +1,22 @@
-console.log('It works!');
+// regularPrint(content) – accepts a string content and prints it to the console
+
+function regularPrint(string) {
+  console.log(string);
+}
+
+// loudPrint(content) – accepts a string content and prints an upcased version of
+// it surrounded by stars (e.g. ***THIS VIDEO SUCKS***)
+
+// omissionCensor(content, badWords) – accepts a string content and an array of
+// strings badWords, and returns a version of content with all instances of the
+// strings in badWords removed
+
+// substitutionCensor(content, badWords) – accepts a string content and an array
+// of strings badWords, and returns a version of content with all instances of
+// the strings in badWords replaced with a random element from an array of
+// "substitute" strings (internally defined in the function)
+
+// publishComment(comment, badWords, censorFunction, printFunction) – accepts a
+// string comment, an array of strings badWords, a function to censor the
+// comment censorFunction, and a function to print the comment printFunction
+
