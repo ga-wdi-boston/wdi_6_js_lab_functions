f353619d68ec8817698c7ef1a3953347f3d93608
EricRho <eric.rho.87@gmail.com>
2014-07-15 20:34:54 -0400

    Create content and badword Array for use

--- comments.js
+++ comments.js
@@ -1,5 +1,8 @@
 console.log('It works!');
 
+content = "Get tae fuck you daft cunt, ken";
+badWords = ['fuck', 'cunt'];
+
 // 1.)
 function regularPrint(content) {
   console.log(content);
@@ -10,3 +13,6 @@ function loudPrint(content) {
   console.log('***' + content.toUpperCase() + '***');
 }
 
+function omissionCensor(content, badWords){
+
+}
