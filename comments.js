770d0277afdcd0414e7a57db2966a58568a61f86
Jodi Detch <jodidetch@gmail.com>
2014-07-15 22:26:01 -0400

    HW has function to accept a string & print it to the console

--- comments.js
+++ comments.js
@@ -1,1 +1,9 @@
-console.log('It works!');
+//Define the following functions:
+
+// `regularPrint(content)` &ndash; accepts a string `content` and prints it to the console
+
+function regularPrint(content) {
+  console.log(content);
+}
+
+
