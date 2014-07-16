8ac8f3dc594d00a1b3e40a82451c1ba28a88fc2a
Jodi Detch <jodidetch@gmail.com>
2014-07-15 22:28:31 -0400

    HW has function to accept a string and prints an upcased version of it surrounded by stars

--- comments.js
+++ comments.js
@@ -6,4 +6,9 @@ function regularPrint(content) {
   console.log(content);
 }
 
+//`loudPrint(content)` &ndash; accepts a string `content` and prints an upcased version of it surrounded by stars (e.g. `***THIS VIDEO SUCKS***`)
+
+function loudPrint(content) {
+  return "***" + content.toUpperCase() + "***";
+}
 
