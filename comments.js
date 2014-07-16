5c092d9e4c2949364b1d084a0eddcb12dd34db76
brimatteng <btenggren20@gmail.com>
2014-07-15 21:49:43 -0400

    First JavaScript homework. I think it went pretty smooth

--- comments.js
+++ comments.js
@@ -1,1 +1,56 @@
-console.log('It works!');
+// content is a string
+function regularPrint(content){
+  console.log(content);
+}
+//content is a string
+function loudPrint(content){
+  console.log("***" + content.toUpperCase() + "***");
+}
+//Made my own printer
+function radioPrint(content){
+  console.log("KKKsssssshhhhhhhkkkk " + content + " Over Kksssssshhhhhhhkkkk");
+}
+
+//content is string, badWords is an array
+function omissionCensor(content, badWords){
+  var splitContent = content.split(' ');
+  badWords.forEach(function(badWord){
+    splitContent.forEach(function(word,i){
+      if (badWord === word){splitContent[i]= '____';}
+    });
+  });
+  return splitContent.join(' ');
+}
+
+// content is string badWords is an arry
+//return content with badWords subbed out for Good ones
+function substitutionCensor(content, badWords){
+  var splitContent = content.split(' ');
+  var goodWords=['tasty','bugger', 'smile-happy guy',
+     'Rick James', 'BoomPow', '#$%#!'];
+
+  badWords.forEach(function(badWord){
+    splitContent.forEach(function(word,i){
+      var replacement = goodWords[Math.floor(Math.random()*goodWords.length)];
+      if (badWord === word){splitContent[i]= replacement;}
+    });
+  });
+  return splitContent.join(' ');
+}
+
+// Made my own censor for meows
+function meowCensor(content, badWords){
+  var splitContent = content.split(' ');
+
+  badWords.forEach(function(badWord){
+    splitContent.forEach(function(word,i){
+      if (badWord === word){splitContent[i]= 'meow';}
+    });
+  });
+  return splitContent.join(' ');
+}
+// puts all them bad larry's together
+function publishComment(comment, badWords, censorFunction, printFunction){
+  var cleanup = censorFunction(comment,badWords);
+  printFunction(cleanup);
+}
